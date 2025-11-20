7122f6e205a187b38b67d590c39b0bb335232ee2
L <114188050+e1berd@users.noreply.github.com>
2025-11-20 16:37:29 +0300

    Modify translate function to use src and dst parameters
    Updated the translate function to accept source and destination languages as arguments.

--- translate.fish
+++ translate.fish
@@ -1,5 +1,14 @@
-function translate --argument text
+function translate --argument src_dst text
+    if not string match -q "*:*" $src_dst
+        echo "Usage: translate ru:en \"текст\""
+        return 1
+    end
+
+    set src (string split ":" $src_dst)[1]
+    set dst (string split ":" $src_dst)[2]
+
     set encoded_text (string escape --style=url $text)
-    set response (curl -s "https://translate.googleapis.com/translate_a/single?client=gtx&sl=ru&tl=en&dt=t&q=$encoded_text")
-    echo $response | jq -r '.[0][0][0]' | string lower
+    set response (curl -s "https://translate.googleapis.com/translate_a/single?client=gtx&sl=$src&tl=$dst&dt=t&q=$encoded_text")
+
+    echo $response | jq -r '.[0][0][0]'
 end
